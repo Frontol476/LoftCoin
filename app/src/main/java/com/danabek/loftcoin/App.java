b4050cdcf1a293e941b7ad35b716794216817fb3
Kartbayev Danabek <danabek55@gmail.com>
2019-03-31 21:37:32 +0600

    RateResponse, Quote

--- app/src/main/java/com/danabek/loftcoin/App.java
+++ app/src/main/java/com/danabek/loftcoin/App.java
@@ -2,21 +2,29 @@ package com.danabek.loftcoin;
 
 import android.app.Application;
 
+import com.danabek.loftcoin.data.api.Api;
+import com.danabek.loftcoin.data.api.ApiInitializer;
 import com.danabek.loftcoin.data.prefs.Prefs;
 import com.danabek.loftcoin.data.prefs.PrefsImpl;
 
 public class App extends Application {
     private Prefs prefs;
+    private Api api;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         prefs = new PrefsImpl(this);
+        api = new ApiInitializer().init();
     }
 
     public Prefs getPrefs() {
 
         return prefs;
+    }
+
+    public Api getApi() {
+        return api;
     }
 }
